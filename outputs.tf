31c41e866434fbbe90c7177e19f002eae1eaa415
IshmeetManik <60575842+IshmeetManik@users.noreply.github.com>
2024-07-24 16:40:22 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "vpcs" {
   description = "VPC Outputs"
   value       = { for vpc in aws_vpc.this : vpc.tags.Name => { "cidr_block" : vpc.cidr_block, "id" : vpc.id } }
 }
+output "instance_id" {
+  value = aws_instance.ssm_instance.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.ssm_instance.public_ip
+}
